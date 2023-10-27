6e8fc6d46d85511c1b6359fcad937a3c87ccf063
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-10-27 13:46:02 -0300

    feat: Requisito 2

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,46 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+/**
+ * Classe que representa uma pessoa candidata.
+ */
+public class PessoaCandidata extends Pessoa {
+  private int numero;
+  private int votos;
 
+  /**
+   * Construtor da classe PessoaCandidata.
+   */
+  public PessoaCandidata(String nome, int numero) {
+    this.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  /**
+   * Getter and Setter.
+   */
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  /**
+   * Método que retorna a quantidade de votos.
+   */
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
 }
