d437687b0cdbb1192074ee21ef1cbd0f65c5029a
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-10-27 13:49:24 -0300

    feat: Requisito 3

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,27 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+/**
+ * Classe que representa uma pessoa eleitora.
+ */
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
 
+  /**
+   * Construtor da classe PessoaEleitora.
+   */
+  public PessoaEleitora(String nome, String cpf) {
+    super.nome = nome;
+    this.cpf = cpf;
+  }
+
+  /**
+   * Getter and Setter.
+   */
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
