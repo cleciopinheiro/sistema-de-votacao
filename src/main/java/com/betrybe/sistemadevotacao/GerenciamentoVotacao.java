ca09a29c0a736b032b1401c4d9fca78bc4141b5c
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-10-27 13:53:41 -0300

    feat: REquisito 4

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,22 @@
 package com.betrybe.sistemadevotacao;
 
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
 
+/**
+ * Classe que representa o gerenciamento de votação.
+ */
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+
+  private ArrayList<PessoaCandidata> cpfsComputados;
+  private ArrayList<PessoaCandidata> pessoasCandidatas;
+  private ArrayList<PessoaEleitora> pessoasEleitoras;
+
+  /**
+   * Construtor da classe GerenciamentoVotacao.
+   */
+  public GerenciamentoVotacao() {
+    this.cpfsComputados = new ArrayList<PessoaCandidata>();
+    this.pessoasCandidatas = new ArrayList<PessoaCandidata>();
+    this.pessoasEleitoras = new ArrayList<PessoaEleitora>();
+  }
 }
