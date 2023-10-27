de2a66453b0fe69f2bc1bd088ec6431af9edb6aa
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-10-27 13:34:12 -0300

    feat: Requisito 1

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,19 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
+/**
+ * Classe abstrata que representa uma pessoa.
+ */
+public abstract class Pessoa {
+  protected String nome;
   
+  /**
+   * Getter and Setter.
+   */
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
