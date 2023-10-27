875d833f671a360e7e40e3f9ba68ada89286ec2c
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-10-27 14:05:02 -0300

    feat: Requisito 5

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -19,4 +19,40 @@ public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
     this.pessoasCandidatas = new ArrayList<PessoaCandidata>();
     this.pessoasEleitoras = new ArrayList<PessoaEleitora>();
   }
+
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    boolean pessoaCandidataJaExiste = false;
+
+    for (PessoaCandidata pessoaCandidata : this.pessoasCandidatas) {
+      if (pessoaCandidata.getNumero() == numero) {
+        pessoaCandidataJaExiste = true;
+        break;
+      }
+    }
+
+    if (pessoaCandidataJaExiste) {
+      System.out.println("Número da pessoa candidata já utilizado!");
+    } else {
+      PessoaCandidata pessoaCandidata = new PessoaCandidata(nome, numero);
+      this.pessoasCandidatas.add(pessoaCandidata);
+    }
+  }
+
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    boolean pessoaEleitoraJaExiste = false;
+
+    for (PessoaEleitora pessoaEleitora : this.pessoasEleitoras) {
+      if (pessoaEleitora.getCpf().equals(cpf)) {
+        pessoaEleitoraJaExiste = true;
+        break;
+      }
+    }
+
+    if (pessoaEleitoraJaExiste) {
+      System.out.println("Pessoa eleitora já cadastrada!");
+    } else {
+      PessoaEleitora pessoaEleitora = new PessoaEleitora(nome, cpf);
+      this.pessoasEleitoras.add(pessoaEleitora);
+    }
+  }
 }
